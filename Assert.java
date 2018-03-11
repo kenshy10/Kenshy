1576c2d11430c464b5131d7c7791884b16812145
kenshy10 <36676992+kenshy10@users.noreply.github.com>
2018-03-11 18:09:37 +0100

    Add files via upload

---
+++ Assert.java
@@ -0,0 +1,10 @@
+package basura2;
+
+public class Assert {
+
+	public static void assertEquals(boolean valid, boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ValidatorUtil.java
@@ -0,0 +1,16 @@
+package basura2;
+
+
+public class ValidatorUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static boolean validateEmail(String temp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
